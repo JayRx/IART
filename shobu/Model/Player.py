70d641a7149e0a382c9bfd6f32b8ceb094596acc
Joao Rocha <jr007silva1306@gmail.com>
2021-04-01 10:03:04 +0100

    Fixed passive mode

--- shobu/Model/Player.py
+++ shobu/Model/Player.py
@@ -70,84 +70,25 @@ class Player:
 
     def calc_moves(self, board, piece):
         self.__moves = []
+        i_list = [-1, 0, 1]
+        j_list = [-1, 0, 1]
         self.__row = piece.get_row()
         self.__col = piece.get_col()
-        for i in range(2):
-            # LEFT
-            if self.__row > i:
-                if i == 1:
-                    if board.get_cell(self.__row - i, self.__col) == 0:
-                        self.__moves.append([self.__row - 1 - i, self.__col])
-                else:
-                    self.__moves.append([self.__row - 1 - i, self.__col])
 
-            # RIGHT
-            if self.__row < ROWS - 1 - i:
-                if i == 1:
-                    if board.get_cell(self.__row + i, self.__col) == 0:
-                        self.__moves.append([self.__row + 1 + i, self.__col])
-                else:
-                    self.__moves.append([self.__row + 1 + i, self.__col])
+        if piece != 0 and piece.get_color() == self.get_color():
+            for i2 in i_list:
+                for j2 in j_list:
+                    if 0 <= self.__row + i2 < ROWS and 0 <= self.__col + j2 < COLS and board.get_cell(self.__row + i2, self.__col + j2) == 0:
+                        self.__moves.append([self.__row + i2, self.__col + j2])
 
-            # DOWN
-            if self.__col > i:
-                if i == 1:
-                    if board.get_cell(self.__row, self.__col - i) == 0:
-                        self.__moves.append([self.__row, self.__col - 1 - i])
-                else:
-                    self.__moves.append([self.__row, self.__col - 1 - i])
+            for i2 in i_list:
+                for j2 in j_list:
+                    if 0 <= self.__row + i2*2 < ROWS and 0 <= self.__col + j2*2 < COLS and board.get_cell(self.__row + i2*2, self.__col + j2*2) == 0 and board.get_cell(self.__row + i2, self.__col + j2) == 0:
+                        self.__moves.append([self.__row + i2*2, self.__col + j2*2])
 
-            # UP
-            if self.__col < COLS - 1 - i:
-                if i == 1:
-                    if board.get_cell(self.__row, self.__col + i) == 0:
-                        self.__moves.append([self.__row, self.__col + 1 + i])
-                else:
-                    self.__moves.append([self.__row, self.__col + 1 + i])
-
-            # LEFT DOWN
-            if self.__row > i and self.__col > i:
-                if i == 1:
-                    if board.get_cell(self.__row - i, self.__col - i) == 0:
-                        self.__moves.append([self.__row - 1 - i, self.__col - 1 - i])
-                else:
-                    self.__moves.append([self.__row - 1 - i, self.__col - 1 - i])
-
-            # RIGHT DOWN
-            if self.__row < ROWS - 1 - i and self.__col > i:
-                if i == 1:
-                    if board.get_cell(self.__row + i, self.__col - i) == 0 and i == 2:
-                        self.__moves.append([self.__row + 1 + i, self.__col - 1 - i])
-                else:
-                    self.__moves.append([self.__row + 1 + i, self.__col - 1 - i])
-
-            # LEFT UP
-            if self.__row > i and self.__col < COLS - 1 - i:
-                if i == 1:
-                    if board.get_cell(self.__row - i, self.__col + i) == 0:
-                        self.__moves.append([self.__row - 1 - i, self.__col + 1 + i])
-                else:
-                    self.__moves.append([self.__row - 1 - i, self.__col + 1 + i])
-
-            # RIGHT UP
-            if self.__row < ROWS - 1 - i and self.__col < COLS - 1 - i:
-                if i == 1:
-                    if board.get_cell(self.__row + i, self.__col + i) == 0:
-                        self.__moves.append([self.__row + 1 + i, self.__col + 1 + i])
-                else:
-                    self.__moves.append([self.__row + 1 + i, self.__col + 1 + i])
-
-        aux_moves = self.__moves.copy()  # evita algum valor ser ignorado , quando removemos vários elementos
-        for move in self.__moves:
-            # imp_moves = []
-            move_row, move_col = move
-            cell = board.get_cell(move_row, move_col)
-            if cell != 0:
-                # imp_moves.append(move.copy())
-                aux_moves.remove(move)
-        self.__moves = aux_moves
-        # Verificar movimentos impossíveis por terem uma peça antes à frente
         return self.__moves
+
+
 
     def set_selected_piece_pos(self, piece):
         self.__row = piece.get_row()
