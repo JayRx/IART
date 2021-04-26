78b5c0923e34706cdfca03cf436109a5766a244f
Joao Rocha <jr007silva1306@gmail.com>
2021-04-26 12:01:05 +0100

    Initialized Proj2!

--- proj2/main.py
+++ proj2/main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+import pandas as pd
+
+humor_data = pd.read_csv('files/dev.csv')
+
+print("\n----- Head -----")
+print(humor_data.head())
+
+print("\n----- Describe ------")
+print(humor_data.describe())
