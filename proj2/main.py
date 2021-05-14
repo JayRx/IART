ae0c1151a982d747298846c2faab1e1574db9ffa
Flávio Vaz <up201509918@fc.up.pt>
2021-05-14 13:36:23 +0100

    com o exercicio exemplo

--- proj2/main.py
+++ proj2/main.py
@@ -1,9 +1,39 @@
+import csv
+
+import nltk
 import pandas as pd
+from nltk.tokenize import *
 
-humor_data = pd.read_csv('files/dev.csv')
 
-print("\n----- Head -----")
-print(humor_data.head())
+# nltk.download('punkt')
 
-print("\n----- Describe ------")
-print(humor_data.describe())
+
+def main():
+    with open("/data/public_dev.csv", newline='') as line:
+        reader = csv.reader(line)
+        data = list(reader)
+    public_text = pd.read_csv("/data/public_dev.csv", header=None)
+
+    # print(data)
+    aux = public_text.iloc[1:, 1:2]
+
+    tokens_list = []
+
+    for phrase in data:
+        # print( phrase[1])
+        tokens = word_tokenize(phrase[1])
+        tokens_list.append(tokens)
+
+    # every single list inside tokens_list have the tokens of one possible joke
+    tokens_list = tokens_list[1:]
+
+    # print(tokens_list)
+    """
+    humor_data = pd.read_csv('files/dev.csv')
+    
+    print("\n----- Head -----")
+    print(humor_data.head())
+    
+    print("\n----- Describe ------")
+    print(humor_data.describe())
+    """
